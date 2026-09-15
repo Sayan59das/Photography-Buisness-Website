1419d26ee28724b7ab2f779415340988fbb629e2
Sayan59das <sayandas052007@gmail.com>
2026-09-15 09:41:32 +0530

    Add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/Photography-Buisness-Website",
   images: {
     unoptimized: true,
     remotePatterns: [
